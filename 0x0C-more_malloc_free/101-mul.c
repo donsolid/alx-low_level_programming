35913e6d40cc173f71e6d5a31bf0a8ad4ac2b2d3
Obichukwu uchenna <uobichukwu@web.de>
2022-05-07 15:09:19 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,30 +1,94 @@
 #include "main.h"
-#include <stdlib.h>
+void insertc(int j, int st, char *arri, char *str);
+int county(char *str);
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
+ * strtow - returns a pointer to an array of strings (words)
+ * @str: string
+ * Return: returns a pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+	int i = 0, j, count = 0, st = -1, z;
+	char **arr;
+
+	if (str == NULL)
+		return (NULL);
+	count = county(str);
+	arr = (char **) malloc(sizeof(char *) * count);
+	if (arr == NULL || count == 0)
+		return (NULL);
+	for (j = 0 ; str[j] != '\0' && i < count; j++)
+	{
+		if (str[j] != ' ' && j == 0)
+			st = j;
+		if (str[j] != ' ' && str[j - 1] == ' ')
+			st = j;
+		if (str[j] != ' ' && (str[j + 1] == ' ' || str[j + 1] == '\0'))
+		{
+			z = j - st + 2;
+			arr[i] = (char *) malloc(sizeof(char) * z);
+			if (arr[i] == NULL)
+			{
+				for (j = 0; j <= i; j++)
+					free(arr[j]);
+				return (NULL);
+				free(arr);
+			}
+			insertc(j, st, arr[i], str);
+			i++;
+		}
+		else
+			continue;
+	}
+	arr[i] = (char *) malloc(1 * 8);
+	if (arr[i] == NULL)
+		return (NULL);
+	arr[i] = NULL;
+	return (arr);
+}
+/**
+ * insertc - insert
+ * @j: j
+ * @st: start point;
+ * @arri: string
+ * @str: str
+ * Return: Void
+ */
+void insertc(int j, int st, char *arri, char *str)
+{
+	int i = 0, z = 0;
+
+	z = j - st + 1;
+	for (i = 0; i < z; i++)
+	{
+		arri[i] = str[st];
+		st++;
+	}
+	arri[i] = '\0';
+}
+/**
+ * county - count words separated by ' '
+ * @str: string
  * Return: int
  */
-int main(int argc, char *argv[])
+int county(char *str)
 {
-unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
+	int i = 0, count = 0;
 
+	while (str[i])
+	{
+		if (str[i] != ' ' && i == 0)
+		{
+			count++;
+			i++;
+			continue;
+		}
+		if (str[i] != ' ' && str[i - 1] == ' ')
+		{
+			count++;
+		}
+		i++;
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
-return (0);
+	return (count);
 }
